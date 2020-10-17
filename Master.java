cfb76025f7bdd203a4438e7094d7b1d8d43d35f9
Yoga Mahendra <4453666-m.yoga.mahendra@users.noreply.gitlab.com>
2020-10-17 10:18:39 +0000

    Update Master.java, Worker.java files

---
+++ Master.java
@@ -0,0 +1,38 @@
+import java.net.*;
+import java.io.*;
+
+class Worker{  
+    public static void main(String args[])throws Exception{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        print("Master IP: ");
+        String masterIP = br.readLine();
+        print("Master Port: ");
+        int masterPort = Integer.parseInt(br.readLine());
+        print("Connecting...");
+        Socket s=new Socket(masterIP,masterPort);  
+        DataInputStream din=new DataInputStream(s.getInputStream());  
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());    
+        print("Connected.");
+        
+        String input="";  
+        while(!input.equals("stop")){  
+            // Input
+            print("Waiting for job...");
+            input=din.readUTF();  
+            print("Job Received. Input:");
+            print(input);
+            print("Sorting...");
+
+            // Output
+            dout.writeUTF(sort(input));  
+            dout.flush();  
+            print("Result has been returned to master.");
+        }  
+        dout.close();  
+        s.close();  
+    }
+    static void print(String s){System.out.println(s);}
+    static String sort(String input){
+        return input;
+    }
+} 
