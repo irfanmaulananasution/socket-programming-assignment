68caf1f71470389762524b24322560ffd2f707f3
Yoga Mahendra <4453666-m.yoga.mahendra@users.noreply.gitlab.com>
2020-10-18 06:49:49 +0000

    Update Master.java

--- Master.java
+++ Master.java
@@ -1,38 +1,27 @@
-import java.net.*;
-import java.io.*;
-
+import java.net.*;  
+import java.io.*;  
 class Master{  
-    public static void main(String args[])throws Exception{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        print("Master IP: ");
-        String masterIP = br.readLine();
-        print("Master Port: ");
-        int masterPort = Integer.parseInt(br.readLine());
-        print("Connecting...");
-        Socket s=new Socket(masterIP,masterPort);  
+    public static void main(String args[])throws Exception{  
+        ServerSocket ss=new ServerSocket(3333);
+        print("Waiting for worker");
+        Socket s=ss.accept();
+        // Socket s2=ss.accept();
+        // Socket s3=ss.accept();
         DataInputStream din=new DataInputStream(s.getInputStream());  
-        DataOutputStream dout=new DataOutputStream(s.getOutputStream());    
-        print("Connected.");
-        
-        String input="";  
-        while(!input.equals("stop")){  
-            // Input
-            print("Waiting for job...");
-            input=din.readUTF();  
-            print("Job Received. Input:");
-            print(input);
-            print("Sorting...");
-
-            // Output
-            dout.writeUTF(sort(input));  
-            dout.flush();  
-            print("Result has been returned to master.");
-        }  
-        dout.close();  
+        DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
+        String result="",order="";  
+        while(!order.equals("stop")){ 
+            System.out.println("Go");
+            order=br.readLine();
+            dout.writeUTF(order);
+            dout.flush();
+            result=din.readUTF();
+            System.out.println("Worker says: "+ result);
+        }
+        din.close();  
         s.close();  
+        ss.close();  
     }
     static void print(String s){System.out.println(s);}
-    static String sort(String input){
-        return input;
-    }
-} 
+}
