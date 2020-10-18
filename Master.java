317c52cb4358f698afc5495be3a23d2b339a01e0
Yoga Mahendra <4453666-m.yoga.mahendra@users.noreply.gitlab.com>
2020-10-18 08:05:07 +0000

    Update Worker.java, Master.java files

--- Master.java
+++ Master.java
@@ -1,27 +1,51 @@
-import java.net.*;  
-import java.io.*;  
-class Master{  
-    public static void main(String args[])throws Exception{  
-        ServerSocket ss=new ServerSocket(3333);
+import java.net.*;
+import java.util.Arrays;
+import java.util.Random;
+import java.io.*;
+
+class Master{
+    static Random rand = new Random();
+
+    public static void main(String args[]) throws Exception {
+        ServerSocket ss = new ServerSocket(3333);
         print("Waiting for worker");
-        Socket s=ss.accept();
+        Socket s = ss.accept();
         // Socket s2=ss.accept();
         // Socket s3=ss.accept();
-        DataInputStream din=new DataInputStream(s.getInputStream());  
-        DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));  
-        String result="",order="";  
-        while(!order.equals("stop")){ 
-            System.out.println("Go");
-            order=br.readLine();
+        DataInputStream din = new DataInputStream(s.getInputStream());
+        DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        print("Connection with worker has been established successfully.");
+        String result = "", order = "";
+        while (true) {
+            print("Waiting for job for worker.");
+            order = br.readLine();
+            if (order.equals("stop"))
+                break;
+            else if (order.split(" ")[0].equals("generate"))
+                order = seeder(order.split(" ")[1]);
             dout.writeUTF(order);
             dout.flush();
-            result=din.readUTF();
-            System.out.println("Worker says: "+ result);
+            print("Job given to Worker. Waiting for result...");
+            result = din.readUTF();
+            print("Result: " + result);
         }
-        din.close();  
-        s.close();  
-        ss.close();  
+        din.close();
+        s.close();
+        ss.close();
+        print("Master is successfully stopped.");
     }
-    static void print(String s){System.out.println(s);}
+
+    static void print(String s) {
+        System.out.println(s);
+    }
+
+    static String seeder(String size) {
+        int max = Integer.parseInt(size);
+        int[] job = new int[max];
+        for (int i = 0; i < max; i++) {
+            job[i] = rand.nextInt(max+1);
+        }
+        return Arrays.toString(job);
+    }
 }
--- Worker.java
+++ Worker.java
@@ -1,5 +1,8 @@
 import java.net.*;
 import java.io.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 
 class Worker{  
     public static void main(String args[])throws Exception{
@@ -15,12 +18,13 @@ class Worker{
         print("Connected.");
         
         String input="";  
-        while(!input.equals("stop")){  
+        while(true){
             // Input
             print("Waiting for job...");
             input=din.readUTF();  
-            print("Job Received. Input:");
+            print("Order Received. Input:");
             print(input);
+            if(input.equals("stop")) break;
             print("Sorting...");
 
             // Output
@@ -28,11 +32,48 @@ class Worker{
             dout.flush();  
             print("Result has been returned to master.");
         }  
+        din.close();
         dout.close();  
         s.close();  
+        print("Worker is successfully stopped.");
     }
+
     static void print(String s){System.out.println(s);}
+
     static String sort(String input){
-        return input;
+        String[] toParse = input.replaceAll("\\[", "").replaceAll("\\]", "").replaceAll("\\s", "").split(",");
+
+        int[] toSort = new int[toParse.length];
+
+        // Parses the input, checking if there's non number in the values.
+        for (int i = 0; i < toSort.length; i++) {
+            try {
+                toSort[i] = Integer.parseInt(toParse[i]);
+            } catch (NumberFormatException nfe) {
+                //NOTE: write something here if you need to recover from formatting errors
+                String err = "The array has invalid value: "+toParse[i]+" at index "+Integer.toString(i);
+                print(err);
+                return err;
+            };
+        }
+        // Sorting algorithm used: Counting sort with Treemap for sorting and storing duplicate values
+        // Source: https://www.techiedelight.com/efficiently-sort-array-duplicated-values/
+		Map<Integer, Integer> freq = new TreeMap<>();
+
+		// store distinct values in the input array as key and their respective counts as values
+		for (int i: toSort) {
+			freq.put(i, freq.getOrDefault(i, 0) + 1);
+		}
+
+		// traverse the sorted map and overwrite the input array with sorted elements
+		int i = 0;
+		for (var entry: freq.entrySet()) {
+			int value = entry.getValue();
+			while (value-- > 0) {
+				toSort[i++] = entry.getKey();
+			}
+		}
+        // Returns the String repr of the sorted array
+        return Arrays.toString(toSort);
     }
 } 
